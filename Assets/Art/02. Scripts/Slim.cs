930049330913dd629b0eb84a67fa90ac2687d94d
libo-liti <000525jh@gamil.com>
2025-07-14 19:44:55 +0900

    Slime Test

---
+++ Assets/Art/02. Scripts/Slim.cs
@@ -0,0 +1,47 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class Slim : MonoBehaviour
+{
+    public TextMeshPro hpText;
+    public TextMeshPro hpBigText;
+    private Vector2 _mousePos;
+    public static int hp = 5;
+
+    private void Start()
+    {
+        hpText.text = hp.ToString();
+        hpBigText.text = hp.ToString();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (_mousePos.x > 1)
+                transform.position = new Vector2(2, -4);
+            else if (_mousePos.x < -1)
+                transform.position = new Vector2(-2, -4);
+            else if(_mousePos.x < 1 && _mousePos.x > -1)
+                transform.position = new Vector2(0, -4);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            if (hp > other.GetComponent<Enemy>().hp)
+            {
+                hp += other.GetComponent<Enemy>().hp;
+                hpText.text = hp.ToString();
+                hpBigText.text = hp.ToString();
+                Destroy(other.gameObject);
+            }
+            else
+                Destroy(gameObject);
+        }
+    }
+}
